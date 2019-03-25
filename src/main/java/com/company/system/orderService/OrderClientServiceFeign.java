b29bc84c1554bb00e160dbbdad7a73283943b4d6
wangjiamin <wangjiamin@jiangmeilong-m4500.bj.xinwei.com.cn>
2019-03-25 19:27:57 +0800

    @FeignClient 写入配置文件

--- src/main/java/com/company/system/orderService/OrderClientServiceFeign.java
+++ src/main/java/com/company/system/orderService/OrderClientServiceFeign.java
@@ -12,28 +12,32 @@ import com.xinwei.orderDb.domain.OrderFlow;
 import com.xinwei.orderDb.domain.OrderMainContext;
 
 // 服务名称不能使用下划线
-@FeignClient(value = "order-access", fallback = OrderClientServiceFeignCallback.class)
+@FeignClient(value = "${coojisu.order-access}", fallback = OrderClientServiceFeignCallback.class)
 public interface OrderClientServiceFeign {
 
 	// @PathVariable注解必须使用value属性指定路径
 	@PostMapping(value = "/orderGateway/{category}/{dbId}/{orderId}/createOrder")
-	public ProcessResult addOrderMain(@PathVariable(value = "category") String category, @PathVariable(value = "dbId") String dbId, @PathVariable(value = "orderId") String orderId,
+	public ProcessResult addOrderMain(@PathVariable(value = "category") String category,
+			@PathVariable(value = "dbId") String dbId, @PathVariable(value = "orderId") String orderId,
 			@RequestBody OrderMainContext orderMain);
 
 	@GetMapping(value = "/orderGateway/{category}/{dbId}/{orderId}/startOrder")
-	public ProcessResult startOrderMain(@PathVariable(value = "category") String category, @PathVariable(value = "dbId") String dbId,
-			@PathVariable(value = "orderId") String orderId);
+	public ProcessResult startOrderMain(@PathVariable(value = "category") String category,
+			@PathVariable(value = "dbId") String dbId, @PathVariable(value = "orderId") String orderId);
 
 	@PostMapping(value = "/orderGateway/{category}/{dbId}/{orderId}/putContextData")
-	public ProcessResult putContextData(@PathVariable(value = "category") String category, @PathVariable(value = "dbId") String dbId,
-			@PathVariable(value = "orderId") String orderId, @RequestBody OrderMainContext orderMainContext);
+	public ProcessResult putContextData(@PathVariable(value = "category") String category,
+			@PathVariable(value = "dbId") String dbId, @PathVariable(value = "orderId") String orderId,
+			@RequestBody OrderMainContext orderMainContext);
 
 	@PostMapping(value = "/orderGateway/{category}/{dbId}/{orderId}/getContextData")
-	public ProcessResult getContextData(@PathVariable(value = "category") String category, @PathVariable(value = "dbId") String dbId,
-			@PathVariable(value = "orderId") String orderId, @RequestBody JsonRequest jsonRequest);
+	public ProcessResult getContextData(@PathVariable(value = "category") String category,
+			@PathVariable(value = "dbId") String dbId, @PathVariable(value = "orderId") String orderId,
+			@RequestBody JsonRequest jsonRequest);
 
 	@PostMapping(value = "/orderGateway/{category}/{dbId}/{orderId}/mJumpToNext")
-	public ProcessResult manualJumpToNextStep(@PathVariable(value = "category") String category, @PathVariable(value = "dbId") String dbId,
-			@PathVariable(value = "orderId") String orderId, @RequestBody OrderFlow orderFlow);
+	public ProcessResult manualJumpToNextStep(@PathVariable(value = "category") String category,
+			@PathVariable(value = "dbId") String dbId, @PathVariable(value = "orderId") String orderId,
+			@RequestBody OrderFlow orderFlow);
 
 }
